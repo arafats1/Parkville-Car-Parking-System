93e7af647f00979aa0f5b88e95da617a24d71164
Alaphat Magezi <87616467+arafats1@users.noreply.github.com>
2023-03-29 17:11:35 +0300

    Update loginPage.js

--- routes/loginPage.js
+++ routes/loginPage.js
@@ -7,7 +7,7 @@ router.get('/', (req,res) => {
     res.render('index');
 });
 
-//The login router checks whether the user is in the database
+//The login router checks whether the user is in the database.
 router.post('/', passport.authenticate('local', { failureRedirect: '/' }),(req, res) => {
 	// console.log("This is the login data", req.body)  prints the user's credentials
 	req.session.user = req.user
